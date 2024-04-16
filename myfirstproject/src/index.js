2e385f108c8102246c79b31920367d0c13ab5e0d
zunobono <zxcrtf245@naver.com>
2024-04-16 13:36:14 +0900

    240416

--- myfirstproject/src/index.js
+++ myfirstproject/src/index.js
@@ -4,9 +4,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
 
+root.render(
+    <div>
+
+        {/* Practice #2. */}
+        <h1>Hi everyone!</h1>
+
+        <hr></hr>
+        
+        {/* Practice #3. */}
+        <h1>Class Name</h1>
+        Mobile Software
+        <h1>Proffesor's Name</h1>
+        Woongsup Kim
+        <h1>List of Students</h1>
+        <li>Junho Kim</li>
+        <li>Hyundo Park</li>
+        <li>Heonyoung Jeong</li>
+        <li>Pansuk Sim</li>
+        <li>Sanha Kim</li>
+
+    </div>
+    
+);
 
 reportWebVitals();
