8e1889713bcf990b7a8d302b33fbf387f022665b
zunobono <zxcrtf245@naver.com>
2024-05-07 15:25:25 +0900

    week 10

---
+++ 2019112148_김준호_w10/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dongguk from './App.js';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Dongguk />, document.getElementById('root'));
+
+// reportWebVitals();
