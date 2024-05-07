7f8316b223a073ecdf09c28ee6e7dc4def6a1d49
zunobono <zxcrtf245@naver.com>
2024-05-07 13:03:31 +0900

    file move

--- myfirstproject/src/index.js
+++ myfirstproject/src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Phone from './week8/practice6.js';
-import StudentInfo from './week8/practice7.js';
-import Container from './week8/practice8.js';
-import Student from './week8/practice9.js';
-import StdInfo_10 from './week8/practice10.js';
-import StdInfo_11 from './week8/practice11.js';
 
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Phone/>, document.getElementById("root6"));
-ReactDOM.render(<StudentInfo/>, document.getElementById("root7"));
-ReactDOM.render(<Container/>, document.getElementById("root8"));
-ReactDOM.render(<Student/>, document.getElementById("root9"));
-ReactDOM.render(<StdInfo_10/>, document.getElementById("root10"));
-ReactDOM.render(<StdInfo_11/>, document.getElementById("root11"));
+
 
 // reportWebVitals();
